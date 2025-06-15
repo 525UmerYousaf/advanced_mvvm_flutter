7726c709fad90a9f6b897b67086c73a0daf5474b
525UmerYousaf <umer.yousaf.587@gmail.com>
2025-06-15 17:39:49 +0500

    Added_Dependency_Injection_App_Module

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,6 +1,11 @@
+import 'package:complete_advanced_flutter/data/data_source/remote_data_source.dart';
+import 'package:complete_advanced_flutter/data/repository/repository_impl.dart';
+import 'package:complete_advanced_flutter/domain/repository/repository.dart';
+import 'package:complete_advanced_flutter/domain/usecase/login_usecase.dart';
 import 'package:complete_advanced_flutter/presentation/login/login_viewmodel.dart';
 import 'package:complete_advanced_flutter/presentation/resources/assets_manager.dart';
 import 'package:complete_advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:complete_advanced_flutter/presentation/resources/routes_manager.dart';
 import 'package:complete_advanced_flutter/presentation/resources/strings_manager.dart';
 import 'package:complete_advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +19,7 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
-  LoginViewModel _viewModel =
-      LoginViewModel(null); // todo pass here login useCase
+  LoginViewModel _viewModel = LoginViewModel(loginUseCase);
 
   TextEditingController _userNameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
@@ -42,9 +46,9 @@ class _LoginViewState extends State<LoginView> {
 
   Widget _getContentWidget() {
     return Scaffold(
+      backgroundColor: ColorManager.white,
       body: Container(
         padding: EdgeInsets.only(top: AppPadding.p100),
-        color: ColorManager.white,
         child: SingleChildScrollView(
           child: Form(
             key: _formKey,
@@ -98,15 +102,47 @@ class _LoginViewState extends State<LoginView> {
                     child: StreamBuilder<bool>(
                       stream: _viewModel.outputIsAllInputsValid,
                       builder: (context, snapshot) {
-                        return ElevatedButton(
-                            onPressed: (snapshot.data ?? false)
-                                ? () {
-                                    _viewModel.login();
-                                  }
-                                : null,
-                            child: Text(AppStrings.login));
+                        return SizedBox(
+                          width: double.infinity,
+                          height: AppSize.s40,
+                          child: ElevatedButton(
+                              onPressed: (snapshot.data ?? false)
+                                  ? () {
+                                      _viewModel.login();
+                                    }
+                                  : null,
+                              child: Text(AppStrings.login)),
+                        );
                       },
-                    ))
+                    )),
+                Padding(
+                  padding: EdgeInsets.only(
+                    top: AppPadding.p8,
+                    left: AppPadding.p28,
+                    right: AppPadding.p28,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, Routes.forgotPasswordRoute);
+                        },
+                        child: Text(AppStrings.forgetPassword,
+                            style: Theme.of(context).textTheme.subtitle2),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacementNamed(
+                              context, Routes.registerRoute);
+                        },
+                        child: Text(AppStrings.registerText,
+                            style: Theme.of(context).textTheme.subtitle2),
+                      )
+                    ],
+                  ),
+                )
               ],
             ),
           ),
